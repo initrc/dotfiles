3999b47e23ced196813e33bba0400ce79dfbe33c
David Shi <DavidSnooker@gmail.com>
2016-01-16 01:43:08 -0800

    Remove Atom compile cache

--- atom/atom/compile-cache/coffee/7f67deb9b732f6520b8def1f967d73bda224e5cd.js
+++
@@ -1,8 +0,0 @@
-(function() {
-
-
-}).call(this);
-
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICIiLAogICJzb3VyY2VSb290IjogIiIsCiAgInNvdXJjZXMiOiBbCiAgICAiL1VzZXJzL2RhdmlkLy5hdG9tL2luaXQuY29mZmVlIgogIF0sCiAgIm5hbWVzIjogW10sCiAgIm1hcHBpbmdzIjogIkFBeUNhO0FBQUE7QUFBQSIKfQ==
-
-//# sourceURL=/Users/david/.atom/init.coffee
